91588e437e76b8ad234f96dd1519e0dd237b5d70
Minh Thong <s3852882@rmit.edu.vn>
2022-09-14 17:47:25 +0700

    #

--- frontend/question1/src/Counter.js
+++ frontend/question1/src/Counter.js
@@ -5,24 +5,23 @@ function Counter() {
 
   const [counter, setCounter] = useState(0);
 
-
   const handleChange = (e) => {
     setNumber(e.target.value);
   };
 
   const plusButton = () => {
     const sum = counter + parseInt(number);
-    setCounter(sum)
+    setCounter(sum);
   };
   const minusButton = () => {
     const sum = counter - parseInt(number);
-    setCounter(sum)
+    setCounter(sum);
   };
 
   return (
     <>
       <div>Counter</div>
-      <input type="number" onChange={handleChange} />
+      <input type="text" values={number} onChange={handleChange} />
       <button onClick={plusButton}>+</button>
       <button onClick={minusButton}>-</button>
       <p>{counter}</p>
